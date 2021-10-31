dd94b243efe56592f028a3e625f6a33cb3da33a0
Jake Demarest-Mays <jake@demarest-mays.com>
2021-10-31 12:01:13 -0600

    Almost done with ch3

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,11 @@
 module Test.MySolutions where
 
 import Prelude
+import Math (sqrt, pi)
+import Data.Int (rem)
+
+diagonal w h = sqrt (w * w + h * h)
+
+circleArea radius = pi * radius * radius
+
+leftoverCents dollars = rem dollars 100
--- exercises/chapter3/src/Data/AddressBook.purs
+++ exercises/chapter3/src/Data/AddressBook.purs
@@ -4,7 +4,7 @@ import Prelude
 
 import Control.Plus (empty)
 import Data.List (List(..), filter, head)
-import Data.Maybe (Maybe)
+import Data.Maybe (Maybe, isJust)
 
 type Address =
   { street :: String
@@ -33,14 +33,38 @@ showEntry entry = entry.lastName <> ", " <>
 emptyBook :: AddressBook
 emptyBook = empty
 
--- This line should have been automatically deleted by resetSolutions.sh. See Chapter 2 for instructions. NOTE TO MAINTAINER: If editing `insertEntry`, remember to also update the non-anchored (and unsimplified) version of this function that is hardcoded in the book text.
+-- -- This line should have been automatically deleted by resetSolutions.sh. See Chapter 2 for instructions. NOTE TO MAINTAINER: If editing `insertEntry`, remember to also update the non-anchored (and unsimplified) version of this function that is hardcoded in the book text.
 insertEntry :: Entry -> AddressBook -> AddressBook
-insertEntry = Cons
+insertEntry entry = Cons entry
 
--- This line should have been automatically deleted by resetSolutions.sh. See Chapter 2 for instructions. NOTE TO MAINTAINER: If editing `findEntry`, remember to also update the non-anchored (and unsimplified) version of this function that is hardcoded in the book text.
+-- -- This line should have been automatically deleted by resetSolutions.sh. See Chapter 2 for instructions. NOTE TO MAINTAINER: If editing `findEntry`, remember to also update the non-anchored (and unsimplified) version of this function that is hardcoded in the book text.
+-- findEntry :: String -> String -> AddressBook -> Maybe Entry
+-- findEntry firstName lastName = head <<< filter filterEntry
+--   where
+--   filterEntry :: Entry -> Boolean
+--   filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
 findEntry :: String -> String -> AddressBook -> Maybe Entry
-findEntry firstName lastName = head <<< filter filterEntry
+findEntry firstName lastName book =
+  book
+  # filter filterEntry
+  # head
   where
   filterEntry :: Entry -> Boolean
   filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
 
+findEntryByStreet :: String -> AddressBook -> Maybe Entry
+findEntryByStreet street book =
+  book
+  # filter (_.address.street >>> eq street)
+  # head
+
+isInBook :: String -> String -> AddressBook -> Boolean
+isInBook firstName lastName book =
+  isJust (findEntry firstName lastName book)
+
+removeDuplicates :: AddressBook -> AddressBook
+removeDuplicates book = book # filter (not <<< isDuplicate)
+  where
+  isDuplicate :: Entry -> Boolean
+  isDuplicate entry =
+    isJust (findEntry (entry.firstName) (entry.lastName) book)
